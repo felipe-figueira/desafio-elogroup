f2e2decd4c9a7d9779dcb9b00661215b67d8c822
felipe-figueira <felipe.figueira13@gmail.com>
2020-03-15 13:45:55 -0300

    validações

---
+++ validacao.js
@@ -0,0 +1,36 @@
+function a() {
+    if (document.getElementById("name").validity.patternMismatch) {
+        alert("Por favor, digite pelo menos um sobrenome");
+    }
+}
+function b() {
+    if (document.getElementById("tel").validity.patternMismatch) {
+        alert("Por favor, digite o telefone nesse formato: 99-99999999");
+    }
+}
+
+function habilitar() {
+    if(document.getElementById('facebook').disabled==true){
+        document.getElementById('facebook').disabled=false
+    }
+    if(document.getElementById('linkedin').disabled==true){
+        document.getElementById('linkedin').disabled=false
+    }
+    if(document.getElementById('instagram').disabled==true){
+        document.getElementById('instagram').disabled=false
+    }
+}
+
+function desabilitar() {
+    if(document.getElementById('facebook').disabled==false){
+        document.getElementById('facebook').disabled=true
+    }
+    if(document.getElementById('linkedin').disabled==false){
+        document.getElementById('linkedin').disabled=true
+    }
+    if(document.getElementById('instagram').disabled==false){
+        document.getElementById('instagram').disabled=true
+    }
+}
+
+$("#tel").mask("00-00000000");
